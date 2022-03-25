9a4d046258d8b0cad6ad7c97aa36ab51616e3531
ParticleG <particle_g@outlook.com>
2022-03-25 13:53:58 +0800

    - Update files

--- main.cpp
+++ main.cpp
@@ -1,6 +1,22 @@
 #include <iostream>
+#include <mailio/message.hpp>
+#include <mailio/pop3.hpp>
+#include <string>
+
+using namespace mailio;
+using namespace std;
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+    try {
+        message msg;
+        msg.line_policy(codec::line_len_policy_t::RECOMMENDED, codec::line_len_policy_t::MANDATORY);
+        pop3s conn("outlook.office365.com", 995);
+        conn.authenticate("test@26f-studio.org", "Passwd123#", pop3s::auth_method_t::LOGIN);
+        conn.fetch(1, msg);
+
+        cout << msg.subject() << endl;
+        cout << msg.content() << endl;
+    } catch (const exception &e) {
+        cout << e.what();
+    }
 }
